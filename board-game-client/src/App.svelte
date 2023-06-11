b24117e1367521f39a0244611b38c94ad39cf00f
SzymonSzczotKivi <szymon.szczot@kivy.pl>
2023-06-11 12:04:41 +0200

    files

---
+++ board-game-client/src/App.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import Header from '../src/components/Header.svelte'
+  import CurrentState from '../src/components/CurrentState.svelte'
+  import Options from '../src/components/Options.svelte'
+</script>
+
+
+<main>
+
+  <div class="container my-5">
+      <Header></Header>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+  <div class="px-4 pt-5 my-5 text-center border-bottom">
+    <CurrentState></CurrentState>
+  </div>
+
+  <div class="b-example-divider"></div>
+
+
+  <div class="container my-5">
+    <div class="row p-4 pb-0 pe-lg-0 pt-lg-5 align-items-center rounded-3 border shadow-lg">
+      <Options></Options>
+    </div>
+  </div>
+
+
+  <div class="b-example-divider mb-0"></div>
+</main>
